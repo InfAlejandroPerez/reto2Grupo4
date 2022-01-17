0557908aadeb29a5bb4b7c5e301981f28eafd82c
MarkelGarcia <markel.garciari@elorrieta-errekamari.com>
2022-01-17 18:02:12 +0100

    servidor java

--- src/Controlador/Servidor.java
+++ src/Controlador/Servidor.java
@@ -1,42 +1,63 @@
 package Controlador;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
-	private final int PUERTO = 5000;
-	private int clientes = 0;
 
-	public void iniciar() {
-		// punto de comunicacion del servidor
-		ServerSocket servidor = null;
-		// punto de comunicacion del cliente
-		Socket cliente = null;
-		
-			try {
 
-				servidor = new ServerSocket(PUERTO);
-
-				System.out.println("Servicio iniciado| Esperando clientes...");
-
-				cliente = servidor.accept();
-
-				System.out.println("Cliente aceptado");
-				clientes++;
-				System.out.println("Número de clientes: " + clientes);
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-
-			}
-	}
+	// declaring required variables
+	private static ServerSocket serverSocket;
+	private static Socket clientSocket;
+	private static InputStreamReader inputStreamReader;
+	private static BufferedReader bufferedReader;
+	private static String message="";
+	  
+	public static void iniciar() {
+	  
+	    try {
+	        // creating a new ServerSocket at port 4444
+	        serverSocket = new ServerSocket(4444); 
+	  
+	    } catch (IOException e) {
+	        System.out.println("Could not listen on port: 4444");
+	    }
+	  
+	    System.out.println("Server started. Listening to the port 4444");
+	  
+	    // we keep listening to the socket's 
+	      // input stream until the message
+	    // "over" is encountered
+	    while (!message.equalsIgnoreCase("over")) {
+	        try {
+	  
+	            // the accept method waits for a new client connection
+	            // and and returns a individual socket for that connection
+	            clientSocket = serverSocket.accept(); 
+	            
+	            // get the inputstream from socket, which will have 
+	              // the message from the clients
+	            inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+	            bufferedReader = new BufferedReader(inputStreamReader);                     
+	             
+	              // reading the message
+	            message = bufferedReader.readLine();
+	  
+	            // printing the message
+	            System.out.println(message);
+	              
+	            // finally it is very important
+	              // that you close the sockets
+	            inputStreamReader.close();
+	            clientSocket.close();
+	  
+	        } catch (IOException ex) {
+	            System.out.println("Problem in message reading");
+	        }
+	     }
+	  }
 
 }
